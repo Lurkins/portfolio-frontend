3b5efcb79bbe41b3eb8ce408c74506a7b1e025ae
Paul Perkins <paulperkins7@gmail.com>
2020-03-02 21:46:21 -0600

    fix card margin in Thumbnail component

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './Thumbnail.css';
-// const projectThumbnail = 'http://placekitten.com/g/500/500';
 
 const Thumbnail = (props) => (
-    <div className="thumbnail-card pb-4 mb-4 card border-dark d-flex justify-content-center align-items-center" onClick={() => props.onOpenModal(props.index)}>
+    <div className="thumbnail-card mb-4 bg-info card border-dark d-flex justify-content-center align-items-center" onClick={() => props.onOpenModal(props.index)}>
         <div>
-            <img className="w-100 h-auto mt-2" src={props.img} alt={props.title}/>
+            <img className="img-fluid" src={props.img} alt={props.title}/>
             <div className="overlay">
                 <p className="font-weight-bold thumbnail-card-description">{props.title}</p>
             </div>
